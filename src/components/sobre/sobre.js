20d4866f37ba427addd0fd349f360b946e610a5d
Breno Arraz <brenogomesarraz@gmail.com>
2023-10-04 02:11:41 -0300

    favicon

--- src/components/sobre/sobre.js
+++ src/components/sobre/sobre.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import euImg from '../images/euImg.png'
 import { BoxImgEu, DivDownloadCV, DownloadCV, ImgEu, SobreH2, SobreP, SobreText, SobreTextSpan, Sobree } from './styled-sobre'
-import CV from '../../assets/Curriculo-Breno-Arraz-dev.pdf'
+import CV from '../../assets/Curriculo-breno-arraz-dev.pdf'
 
 export const Sobre = () => {
   return (
@@ -13,7 +13,7 @@ export const Sobre = () => {
       <SobreH2>Sobre Mim</SobreH2>
       <SobreP>Olá! Sou Breno Arraz, um apaixonado desenvolvedor web com uma forte base técnica e uma paixão por criar experiências digitais incríveis. Fui fascinado pelo potencial da web para transformar ideias em realidade. Durante minha jornada, aprimorei meu conhecimento em  <SobreTextSpan> JavaScript, HTML5, CSS3 </SobreTextSpan> e frameworks como <SobreTextSpan>React</SobreTextSpan> e futuramente <SobreTextSpan>Node.js</SobreTextSpan>.</SobreP>
       <DivDownloadCV>
-      <DownloadCV href={CV} target="_blank" download='Curriculo-Breno-Arraz-dev' rel="noopener noreferrer">Download CV</DownloadCV>
+      <DownloadCV href={CV} target="_blank" download='Curriculo-breno-arraz-dev' rel="noopener noreferrer">Download CV</DownloadCV>
       </DivDownloadCV>
       </SobreText>
     
